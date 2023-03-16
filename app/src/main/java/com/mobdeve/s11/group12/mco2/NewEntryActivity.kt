901f961e6969f8e7acf83f04f4b69eadd715cd68
saiko-png <120616557+saiko-png@users.noreply.github.com>
2023-03-16 21:07:38 +0800

    newEntryDone

---
+++ app/src/main/java/com/mobdeve/s11/group12/mco2/NewEntryActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s11.group12.mco2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewEntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_entry)
+    }
+}
