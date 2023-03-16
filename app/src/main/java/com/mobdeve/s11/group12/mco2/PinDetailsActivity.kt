9357cd6f1779ac7a58ef875af9b42542e67275bd
sssamu <maezy_manuel@dlsu.edu.ph>
2023-03-16 21:40:02 +0800

    Intent to pinDetails

---
+++ app/src/main/java/com/mobdeve/s11/group12/mco2/PinDetailsActivity.kt
@@ -0,0 +1,16 @@
+package com.mobdeve.s11.group12.mco2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.s11.group12.mco2.databinding.PinDetailsBinding
+
+class PinDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewBinding: PinDetailsBinding = PinDetailsBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+
+    }
+}
